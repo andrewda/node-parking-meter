ea9d2813b3a9b40fe65cf50fca93de663ac3df38
Andrew <dassonville.andrew@gmail.com>
2016-02-12 22:02:32 +0000

    Add payment + many other additions

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-var http = require("http");
-var url = require("url");
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
 var beacon = require("eddystone-beacon");
 var handlebars = require("handlebars");
+var MongoClient = require("mongodb").MongoClient;
 var fs = require("fs");
 var stripe = require("stripe")(
     "sk_test_key"
@@ -15,69 +17,188 @@ try {
     throw err;
 }
 
+var port = process.env.PORT || options.port;
+
 var layout = handlebars.compile(fs.readFileSync("./layout.html", "utf8"));
 var remain = handlebars.compile(fs.readFileSync("./remain.html", "utf8"));
 
-http.createServer(function(req, res) {
-    var command = url.parse(req.url).pathname.slice(1);
-    var query = url.parse(req.url, true).query;
+MongoClient.connect("mongodb://" + options.mongodb.username + ":" + options.mongodb.password + "@" + options.mongodb.database, function(err, db) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("MongoDB connected");
 
-    res.writeHead(200, {
-        "Content-Type": "text/html"
-    });
+        app.get("/meter/:meter", function(req, res) {
+            var cursor = db.collection("meters").find({
+                "meter": req.params.meter
+            });
 
-    console.log(command);
-    console.log(query);
-    if (command === "pay" && (query.minutes && query.card && query.exp_month && query.exp_year && query.cvc)) {
-        console.log("user is paying with card number:", query.card)
+            cursor.each(function(err, doc) {
+                if (doc !== null) {
+                    console.log(doc);
 
-        stripe.charges.create({
-            amount: parseInt((query.minutes * 1 / 15) * 100),
-            currency: "usd",
-            source: {
-                exp_month: query.exp_month,
-                exp_year: query.exp_year,
-                number: query.card,
-                //number: "4242424242424242",
-                object: "card",
-                cvc: query.cvc
-            },
-            description: "Parking meter charge"
-        }, function(err, charge) {
-            if (err) {
-                console.log(err.message);
-                res.end(layout({
-                    "error": err.message,
-                    "pay_success": false
-                }));
+                    var startTime = parseInt(doc.startTime);
+                    var totalTime = parseInt(doc.totalTime);
+
+                    var seconds = startTime - Math.floor(Date.now() / 1000) + totalTime;
+                    var minutes = Math.round(seconds / 60);
+
+                    console.log(minutes);
+
+                    if (minutes > 0) {
+                        res.end(remain({
+                            "error": false,
+                            "startTime": startTime,
+                            "totalTime": totalTime,
+                            "time": hour(minutes) + ":" + minute(minutes)
+                        }));
+                    } else {
+                        res.end(remain({
+                            "error": false,
+                            "startTime": 0,
+                            "totalTime": 0,
+                            "time": hour(0) + ":" + minute(0)
+                        }));
+                    }
+                } else {
+                    res.end(remain({
+                        "error": true,
+                        "startTime": 0,
+                        "totalTime": 0,
+                        "time": "0:00"
+                    }));
+                }
+            });
+        });
+
+        app.post("/meter/:meter", function(req, res) {
+            var query = req.body;
+
+            if (query.minutes && query.card && query.exp_month && query.exp_year && query.cvc) {
+                console.log("user is paying with card number:", query.card);
+
+                stripe.charges.create({
+                    amount: parseInt((query.minutes * 1 / 15) * 100),
+                    currency: "usd",
+                    source: {
+                        exp_month: query.exp_month,
+                        exp_year: query.exp_year,
+                        number: query.card,
+                        object: "card",
+                        cvc: query.cvc
+                    },
+                    description: "Parking meter charge"
+                }, function(err, charge) {
+                    if (err) {
+                        console.log(err.message);
+                        res.redirect("/?err=" + encodeURI(err.message));
+                    } else {
+                        console.log(charge);
+
+                        db.collection("meters").find({
+                            "meter": req.params.meter
+                        }).toArray(function(err, docs) {
+                            if (err) {
+                                console.log(err);
+                            } else {
+                                if (docs.length > 0) {
+                                    db.collection("meters").update({
+                                        "meter": req.params.meter
+                                    }, {
+                                        "meter": req.params.meter,
+                                        "startTime": Math.floor(Date.now() / 1000),
+                                        "totalTime": query.minutes * 60
+                                    });
+                                } else {
+                                    db.collection("meters").insert({
+                                        "meter": req.params.meter,
+                                        "startTime": Math.floor(Date.now() / 1000),
+                                        "totalTime": query.minutes * 60
+                                    });
+                                }
+
+                                finish();
+                            }
+                        });
+                    }
+                });
             } else {
-                console.log(charge);
-                res.end(remain({
-                    "minutes": query.minutes,
-                    "time": hour(query.minutes) + ":" + minute(query.minutes)
-                }));
+                finish();
+            }
+
+            function finish() {
+                var cursor = db.collection("meters").find({
+                    "meter": req.params.meter
+                });
+
+                cursor.each(function(err, doc) {
+                    if (doc !== null) {
+                        console.log(doc);
+
+                        var startTime = parseInt(doc.startTime);
+                        var totalTime = parseInt(doc.totalTime);
+
+                        var seconds = startTime - Math.floor(Date.now() / 1000) + totalTime;
+                        var minutes = Math.round(seconds / 60);
+
+                        if (minutes > 0) {
+                            res.end(remain({
+                                "error": false,
+                                "startTime": startTime,
+                                "totalTime": totalTime,
+                                "time": hour(minutes) + ":" + minute(minutes)
+                            }));
+                        } else {
+                            res.end(remain({
+                                "error": false,
+                                "startTime": 0,
+                                "totalTime": 0,
+                                "time": hour(0) + ":" + minute(0)
+                            }));
+                        }
+                    } else {
+                        res.end(remain({
+                            "error": true,
+                            "startTime": 0,
+                            "totalTime": 0,
+                            "time": "0:00"
+                        }));
+                    }
+                });
             }
         });
-    } else {
-        res.end(layout({
-            "error": "",
-            "pay_success": false
-        }));
-    }
-}).listen(options.port);
 
-function hour(minutes) {
-    return Math.floor(minutes / 60)
+        app.get("/", function(req, res) {
+            var query = req.query;
+
+            res.end(layout({
+                "error": query.err ? query.err : "undefined"
+            }));
+        });
+    }
+});
+
+app.use(express.static(__dirname + "/public"));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+function hour(t) {
+    return Math.floor(t / 60);
 }
 
-function minute(minutes) {
-    var minutes = Math.floor(minutes % 60);
-
-    if (minutes < 10) {
-        return "0" + Math.floor(minutes % 60);
+function minute(t) {
+    if (t < 10) {
+        return "0" + Math.floor(t % 60);
     } else {
-        return Math.floor(minutes % 60);
+        return Math.floor(t % 60);
     }
 }
+
+app.listen(port, function() {
+    console.log("Listening on port", port);
+});
 
 beacon.advertiseUrl(options.url);
---
+++ public/js/main.js
@@ -0,0 +1,82 @@
+var time = 0;
+
+function addTime() {
+    if (time < 210) {
+        time = time + 5;
+    }
+
+    updateMinutes(time);
+}
+
+function removeTime() {
+    if (time >= 5) {
+        time = time - 5;
+    }
+
+    updateMinutes(time);
+}
+
+function updateMinutes(t) {
+    if (t >= 0) {
+        document.getElementById("time").innerHTML = hour(t) + ":" + minute(t);
+    }
+}
+
+function hour(minutes) {
+    return Math.floor(minutes / 60)
+}
+
+function minute(minutes) {
+    if (Math.floor(minutes % 60) < 10) {
+        return "0" + Math.floor(minutes % 60);
+    } else {
+        return Math.floor(minutes % 60);
+    }
+}
+
+function getTotalMinutes(formattedTime) {
+    return parseInt(formattedTime.split(":")[0] * 60) + parseInt(formattedTime.split(":")[1]);
+}
+
+function displayAlert() {
+    swal({
+        title: "Payment",
+        html: "<p><input id="meter" name="meter" placeholder="Meter Number"></input><br><input id="card" name="card" placeholder="Card Number"></input><br><input id="cvc" name="cvc" placeholder="Card CVC"></input><br><input id="exp_month" name="exp_month" placeholder="Expiration Month"></input><br><input id="exp_year" name="exp_year" placeholder="Expiration Year"></input>",
+        showCancelButton: true,
+        closeOnConfirm: false
+    }, function() {
+        if ($("#meter").val() && $("#card").val() && $("#cvc").val() && $("#exp_month").val() && $("#exp_year").val()) {
+            post("/meter/" + $("#meter").val(), {
+                "card": $("#card").val(),
+                "cvc": $("#cvc").val(),
+                "exp_month": $("#exp_month").val(),
+                "exp_year": $("#exp_year").val(),
+                "minutes": getTotalMinutes($("#time").text())
+            });
+        } else {
+            swal("Error", "Please make sure all information was entered correctly.", "error");
+        }
+    });
+}
+
+function post(path, params, method) {
+    method = method || "post";
+
+    var form = document.createElement("form");
+    form.setAttribute("method", method);
+    form.setAttribute("action", path);
+
+    for (var key in params) {
+        if (params.hasOwnProperty(key)) {
+            var hiddenField = document.createElement("input");
+            hiddenField.setAttribute("type", "hidden");
+            hiddenField.setAttribute("name", key);
+            hiddenField.setAttribute("value", params[key]);
+
+            form.appendChild(hiddenField);
+        }
+    }
+
+    document.body.appendChild(form);
+    form.submit();
+}
