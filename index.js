c7eb38406d95616f24de42e94234bea3fd9dbfc7
Andrew Dassonville <andrewda@users.noreply.github.com>
2016-02-24 13:53:19 -0800

    Add license notification

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+/**
+ * Node-Parking-Meter
+ * A web app that allows users to pay for parking meters around town.
+ * Copyright (C) 2016  Andrew Dassonville
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
